400cdc6e5cc28ebe281e095455ab3e75f40912c7
Tim Peoples <tim@toolman.org>
2018-11-26 11:09:12 -0800

    Update "vsi" command to use viql

--- cmd/vsi/main.go
+++ cmd/vsi/main.go
@@ -12,7 +12,7 @@ import (
 	"github.com/spf13/pflag"
 
 	"toolman.org/file/viminfo"
-	"toolman.org/flags/tristate"
+	"toolman.org/file/viminfo/viql"
 )
 
 func main() {
@@ -34,7 +34,7 @@ func run() error {
 
 	for _, sf := range i.files {
 		if err = i.inspect(sf); err != nil {
-			if !i.cont {
+			if viminfo.IsFatal(err) && !i.cont {
 				return err
 			}
 			i.warn("%s: %v\n", sf, err)
@@ -47,50 +47,48 @@ func run() error {
 type marshaller func(*viminfo.VimInfo) ([]byte, error)
 
 type vsInspector struct {
-	user     string
-	host     string
-	modified tristate.TriState
-	running  tristate.TriState
-	files    []string
-	tmpl     *template.Template
-	marshal  marshaller
-	thisHost string
-	term     byte
-	cont     bool
-	quiet    bool
+	files   []string
+	expr    viql.Expression
+	tmpl    *template.Template
+	marshal marshaller
+	term    byte
+	cont    bool
+	quiet   bool
 }
 
 func inspector() (*vsInspector, error) {
 	var (
-		base = filepath.Base(os.Args[0])
+		name = filepath.Base(os.Args[0])
 		i    = new(vsInspector)
 		err  error
 	)
 
-	fs := pflag.NewFlagSet(base, pflag.ContinueOnError)
+	fs := pflag.NewFlagSet(name, pflag.ContinueOnError)
 
 	var (
+		query  = fs.StringP("query", "q", "all", "Query statement used for selecting output")
 		format = fs.StringP("format", "f", "{{.SwapFile}}", "Output format for VimInfo as a text/template")
 		zero   = fs.BoolP("zero", "0", false, "Output records terminated by a null byte instead of a newline")
 		rjs    = fs.BoolP("json", "j", false, "Output JSON instead of text as defined by --format")
+		qlh    = fs.Bool("help-viql", false, "Show help on how to write query statements")
 	)
 
-	fs.StringVarP(&i.user, "user", "u", "", "Only emit output for files edited by `username`")
-	fs.StringVarP(&i.host, "host", "h", "", "Only emit output for files edited on `hostname`")
-
-	fs.BoolVarP(&i.cont, "continue", "c", i.cont, "Continue on errors reading swap files")
-	fs.BoolVarP(&i.quiet, "quiet", "q", i.quiet, "Emit no file processing errors or warnings")
-
-	tristate.FlagVarPFS(fs, &i.modified, "modified", "m", i.modified, "Only emit output for swap files with a modified flag matching this `TriState` value")
-	tristate.FlagVarPFS(fs, &i.running, "running", "r", i.running, "Only emit output if the associated edit session's run state matches this `TriState` value")
+	fs.BoolVarP(&i.cont, "continue", "c", i.cont, "Continue on errors")
+	fs.BoolVarP(&i.quiet, "silent", "s", i.quiet, "Emit no file processing errors or warnings")
 
 	fs.Usage = func() {
-		fmt.Fprintf(os.Stderr, "USAGE: %s {flags} swapfile [...]\n\n", base)
+		fmt.Fprintf(os.Stderr, "USAGE: %s [flags] swapfile [...]\n\n", name)
 		fs.PrintDefaults()
 	}
 
 	if err = fs.Parse(os.Args); err != nil {
 		return nil, err
+	}
+
+	if *qlh {
+		fs.Usage()
+		viql.Help(os.Stderr)
+		os.Exit(1)
 	}
 
 	if args := fs.Args(); len(args) < 2 {
@@ -99,16 +97,14 @@ func inspector() (*vsInspector, error) {
 		i.files = args[1:]
 	}
 
-	if i.running.IsSet() {
-		if i.thisHost, err = os.Hostname(); err != nil {
-			return nil, fmt.Errorf("connot determine current hostname: %v", err)
-		}
+	if i.expr, err = viql.Compile(*query); err != nil {
+		return nil, fmt.Errorf("bad query statement: %v", err)
 	}
 
 	if *rjs {
 		i.marshal = i.marshalJSON
 	} else {
-		if i.tmpl, err = template.New(base).Parse(*format); err != nil {
+		if i.tmpl, err = template.New(name).Parse(*format); err != nil {
 			return nil, fmt.Errorf("bad output format: %v", err)
 		}
 		i.marshal = i.marshalText
@@ -141,8 +137,8 @@ func (i *vsInspector) inspect(swapfile string) error {
 }
 
 func (i *vsInspector) emit(vi *viminfo.VimInfo) error {
-	if i.filter(vi) {
-		return nil
+	if match, err := i.expr.Evaluate(vi); err != nil || !match {
+		return err
 	}
 
 	b, err := i.marshal(vi)
@@ -154,27 +150,6 @@ func (i *vsInspector) emit(vi *viminfo.VimInfo) error {
 
 	_, err = os.Stdout.Write(b)
 	return err
-}
-
-func (i *vsInspector) filter(vi *viminfo.VimInfo) bool {
-	if i.user != "" && vi.User != i.user {
-		return true
-	}
-
-	if i.host != "" && vi.Hostname != i.host {
-		return true
-	}
-
-	if !i.modified.Match(vi.Modified, true) {
-		return true
-	}
-
-	if rp := i.running.Bool(); rp != nil {
-		err := vi.Running()
-		return (err == nil) == *rp
-	}
-
-	return false
 }
 
 func (i *vsInspector) marshalJSON(vi *viminfo.VimInfo) ([]byte, error) {
